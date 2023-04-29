dea386ebb870e8f777e4e187e6758a872bbc1b65
MartinaJenifer <122343282+MartinaJenifer@users.noreply.github.com>
2023-04-29 15:13:54 +0530

    commit

---
+++ myfirstnode/index.js
@@ -0,0 +1,25 @@
+var express= require('express');
+var app = express();
+var student= require('./student')
+
+app.listen(5050,()=>{ console.log("Server running!");})
+//get-select * from emp 
+//post-insert or write data into the database
+app.get('/',(request, response)=>{
+// response.write("Welcome home!<br>");
+// response.write(student[0].sid.toString());
+response.json(student);
+response.end();
+})
+
+app.get('/aboutus',(request,response)=>{
+    response.send("<b> About us </b>");
+})
+
+app.get('/contact',(request,response)=>{
+response.sendFile(__dirname+'/test.html');
+})
+
+app.post('/contact',(request,response)=>{
+    response.send("Details captured!")
+})
